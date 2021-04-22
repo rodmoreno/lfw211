d21b7cb3693b49d1eb5ebd814ecf7d3f3c47fcd6
Rodrigo Moreno <rodrigo@paywithflux.com>
2021-04-22 02:58:12 -0500

    feat(Chapter 8 - Labs 2): complete code
    example of serial execution using promise

---
+++ chapter-8/labs-2/serial.js
@@ -0,0 +1,37 @@
+'use strict'
+const { promisify } = require('util')
+
+const print = (err, contents) => {
+  if (err) console.error(err)
+  else console.log(contents)
+}
+
+const opA = (cb) => {
+  setTimeout(() => {
+    cb(null, 'A')
+  }, 500)
+}
+
+const opB = (cb) => {
+  setTimeout(() => {
+    cb(null, 'B')
+  }, 250)
+}
+
+const opC = (cb) => {
+  setTimeout(() => {
+    cb(null, 'C')
+  }, 125)
+}
+
+const a = promisify(opA)
+const b = promisify(opB)
+const c = promisify(opC)
+
+async function run() {
+  print(await a())
+  print(await b())
+  print(await c())
+}
+
+run()
