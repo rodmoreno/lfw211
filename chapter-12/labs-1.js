695cd7a8d5a2f66aaed63f79b5484e7428aac15b
Rodrigo Moreno <rodrigo@paywithflux.com>
2021-04-24 02:20:19 -0500

    feat(Chapter 12 - Labs 1): send data to writable stream

---
+++ chapter-12/labs-1.js
@@ -0,0 +1,30 @@
+'use strict'
+const { Readable, Writable } = require('stream')
+const assert = require('assert')
+const createWritable = () => {
+  const sink = []
+  let piped = false
+  setImmediate(() => {
+    assert.strictEqual(piped, true, 'use the pipe method')
+    assert.deepStrictEqual(sink, ['a', 'b', 'c'])
+  })
+  const writable = new Writable({
+    decodeStrings: false,
+    write(chunk, enc, cb) {
+      sink.push(chunk)
+      cb()
+    },
+    final() {
+      console.log('passed!')
+    }
+  })
+  writable.once('pipe', () => {
+    piped = true
+  })
+  return writable
+}
+const readable = Readable.from(['a', 'b', 'c'])
+const writable = createWritable()
+
+readable.pipe(writable)
+
