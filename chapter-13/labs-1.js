b565c09bbd36ae73349ed8e6d257300cb418efd3
Rodrigo Moreno <rodrigo@paywithflux.com>
2021-04-27 00:15:46 -0500

    feat(Chapter 13 - Labs 1): complete the code
    Print list project files into out.txt

---
+++ chapter-13/labs-1.js
@@ -0,0 +1,28 @@
+'use strict'
+const assert = require('assert')
+const { join, basename } = require('path')
+const fs = require('fs')
+const project = join(__dirname, 'project')
+try { fs.rmdirSync(project, {recursive: true}) } catch (err) {}
+const files = Array.from(Array(5), () => {
+  return join(project, Math.random().toString(36).slice(2))
+})
+files.sort()
+fs.mkdirSync(project)
+for (const f of files) fs.closeSync(fs.openSync(f, 'w'))
+
+const out = join(__dirname, 'out.txt')
+
+function exercise () {
+  const projectFiles = fs.readdirSync(project)
+
+  fs.writeFileSync(out, projectFiles.join(','))
+}
+
+exercise()
+assert.deepStrictEqual(
+  fs.readFileSync(out).toString().split(',').map((s) => s.trim()),
+  files.map((f) => basename(f))
+)
+console.log('passed!')
+
